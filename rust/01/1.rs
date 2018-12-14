d4ecaac894699988006666daa13b8df5d8e70d78
Kenny Zhang <busstopbench@gmail.com>
2018-12-14 17:04:06 -0600

    Day 1 part 2 in rust

--- rust/01/1.rs
+++ rust/01/1.rs
@@ -1,13 +1,40 @@
 use std::io::{self, BufRead};
+use std::collections::HashSet;
 
 fn main() {
+    // Part 1
     let mut freq = 0;
-
     let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let x = line.unwrap().parse::<i32>().unwrap();
-        freq += x;
-    }
+    let mut changes = Vec::new();
 
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+        let delta = line.parse::<i32>().unwrap();
+        changes.push(delta);
+        freq += delta;
+    }
     println!("End frequency: {}", freq);
+
+    // Part 2
+    let mut seen: HashSet<i32> = HashSet::new();
+    let mut first_repeat = 0;
+    let mut found_repeat = false;
+    
+    freq = 0;
+    seen.insert(0);
+    while !found_repeat {
+        for delta in &changes {
+            freq += delta;
+            
+            if seen.contains(&freq) {
+                first_repeat = freq;
+                found_repeat = true;
+                break;
+            }
+            else {
+                seen.insert(freq);
+            } 
+        }
+    }
+    println!("First Repeat: {}", first_repeat);
 }
